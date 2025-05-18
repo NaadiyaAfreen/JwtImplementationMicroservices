c2fbc370a5a462bfe2393afde0aed541afc71679
NaadiyaAfreen <88364561+NaadiyaAfreen@users.noreply.github.com>
2025-05-18 11:46:59 +0530

    Update RestaurantController.java

--- restaurant-service/src/main/java/com/microservices/restaurantservice/controller/RestaurantController.java
+++ restaurant-service/src/main/java/com/microservices/restaurantservice/controller/RestaurantController.java
@@ -1,4 +1,4 @@
-package com.microservices.restaurantservice.controller;
+/*package com.microservices.restaurantservice.controller;
 
 import com.microservices.restaurantservice.dto.OrderResponseDTO;
 import com.microservices.restaurantservice.service.RestaurantService;
@@ -24,4 +24,33 @@ public class RestaurantController {
     public OrderResponseDTO getOrder(@PathVariable String orderId) {
         return service.getOrder(orderId);
     }
+} */
+
+package com.microservices.restaurantservice.controller;
+
+import com.microservices.restaurantservice.dto.OrderResponseDTO;
+import com.microservices.restaurantservice.service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/restaurant")
+public class RestaurantController {
+
+    @Autowired
+    private RestaurantService service;
+
+    @GetMapping
+    public String greetingMessage() {
+        return service.greeting();
+    }
+
+    @GetMapping("/orders/status/{orderId}")
+    public OrderResponseDTO getOrder(@PathVariable String orderId, HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        return service.getOrder(orderId, token);
+    }
 }
+
