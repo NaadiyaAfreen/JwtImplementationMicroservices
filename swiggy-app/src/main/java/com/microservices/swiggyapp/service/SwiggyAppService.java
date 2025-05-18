cd2fa362a2b3b2f7a97989729a71b2aa5334afe9
NaadiyaAfreen <88364561+NaadiyaAfreen@users.noreply.github.com>
2025-05-18 11:48:24 +0530

    Update SwiggyAppService.java

--- swiggy-app/src/main/java/com/microservices/swiggyapp/service/SwiggyAppService.java
+++ swiggy-app/src/main/java/com/microservices/swiggyapp/service/SwiggyAppService.java
@@ -1,4 +1,4 @@
-package com.microservices.swiggyapp.service;
+/*package com.microservices.swiggyapp.service;
 
 import com.microservices.swiggyapp.client.RestaurantServiceClient;
 import com.microservices.swiggyapp.dto.OrderResponseDTO;
@@ -18,4 +18,30 @@ public class SwiggyAppService {
     public OrderResponseDTO checkOrderStatus(String orderId) {
         return restaurantServiceClient.fetchOrderStatus(orderId);
     }
+} */
+
+package com.microservices.swiggyapp.service;
+
+import com.microservices.swiggyapp.client.RestaurantServiceClient;
+import com.microservices.swiggyapp.dto.OrderResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Service
+public class SwiggyAppService {
+
+    @Autowired
+    private RestaurantServiceClient restaurantServiceClient;
+
+    public String greeting() {
+        return "Welcome to Swiggy App Service";
+    }
+
+    public OrderResponseDTO checkOrderStatus(String orderId, HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+        return restaurantServiceClient.fetchOrderStatus(orderId, token);
+    }
 }
+
