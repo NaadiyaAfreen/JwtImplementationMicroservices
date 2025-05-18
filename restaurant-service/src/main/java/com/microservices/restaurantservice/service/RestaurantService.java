30f0bc7e854915cce71373286c4496071d0b1045
NaadiyaAfreen <88364561+NaadiyaAfreen@users.noreply.github.com>
2025-05-18 11:47:34 +0530

    Update RestaurantService.java

--- restaurant-service/src/main/java/com/microservices/restaurantservice/service/RestaurantService.java
+++ restaurant-service/src/main/java/com/microservices/restaurantservice/service/RestaurantService.java
@@ -1,4 +1,4 @@
-package com.microservices.restaurantservice.service;
+/*package com.microservices.restaurantservice.service;
 
 import com.microservices.restaurantservice.dao.RestaurantOrderDAO;
 import com.microservices.restaurantservice.dto.OrderResponseDTO;
@@ -17,4 +17,40 @@ public class RestaurantService {
     public OrderResponseDTO getOrder(String orderId) {
         return orderDAO.getOrders(orderId);
     }
+} */
+
+package com.microservices.restaurantservice.service;
+
+import com.microservices.restaurantservice.dto.OrderResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.*;
+
+@Service
+public class RestaurantService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public String greeting() {
+        return "Welcome to Restaurant Service";
+    }
+
+    public OrderResponseDTO getOrder(String orderId, String token) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", token);
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<OrderResponseDTO> response = restTemplate.exchange(
+            "http://order-service/api/orders/" + orderId,
+            HttpMethod.GET,
+            entity,
+            OrderResponseDTO.class
+        );
+
+        return response.getBody();
+    }
 }
+
